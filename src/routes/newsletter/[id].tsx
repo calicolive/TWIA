2531ac1aea737eb17015e8040f6196b132d7a145
Calicolive <calicodeep@gmail.com>
2023-06-22 20:35:21 -0500

    added sign up form to posts

--- src/routes/newsletter/[id].tsx
+++ src/routes/newsletter/[id].tsx
@@ -7,13 +7,14 @@ import {
   useParams,
   useRouteData,
 } from 'solid-start';
+import EmailSignup from '~/components/EmailSignup';
 import Spinner from '~/components/Spinner';
 import { getPost } from '~/lib/sanity';
 
 export function routeData() {
-  return createRouteData(() => {
+  return createRouteData(async () => {
     const params = useParams<{ id: string }>();
-    return getPost(params.id);
+    return await getPost(params.id);
   });
 }
 const Issue: Component<{}> = (props) => {
@@ -25,7 +26,16 @@ const Issue: Component<{}> = (props) => {
         <Head>
           <Title>{post().title}</Title>
         </Head>
-        <main class='  min-h-screen justify-center  '>
+        <main class='  container mx-auto flex min-h-screen flex-col items-center justify-center '>
+          <div class='mt-6 flex w-full grow flex-col items-center justify-center text-center'>
+            <h1 class='bg-zinc-50 bg-grain-dark bg-clip-text  text-4xl font-extrabold tracking-tighter text-transparent '>
+              This Week in Audio
+            </h1>
+            <p class='mt-2 leading-8 text-zinc-100'>
+              Your Weekly Digest of Audio Deals & News
+            </p>
+            <EmailSignup />
+          </div>
           <section class='md:prose-md container prose prose-sm prose-zinc mx-auto mt-12 p-4 text-zinc-50 lg:prose-lg  prose-h2:text-zinc-300 prose-a:text-indigo-500 prose-a:no-underline hover:prose-a:text-indigo-400'>
             <h1 class=' font-bold text-zinc-50 '>{post().title}</h1>
             <PortableText value={post().body} />
